b1a4a399dfedca9410b56c802c76a488108f769b
ZiqiangYoung <ziqiangyoung@foxmail.com>
2023-04-08 16:51:11 +0800

    test(test_translator): update and correct test
    update test:
    补充了一部分反例，
    部分参考了data drive的思想，对代码完成了抽象。
    将提取测试数据为变量，完成复用。
    correct test:
    之前关于 reflection 的一次提交中，
    出于设计上的考量，修改了一个方法的签名（尤其返回值）
    并将该函数原来的功能变更到了一个新的函数
    这一项破坏性变更，无法被静态检查发现。
    (parameterized typy hint不完善)，
    导致以前接入了该方法的测试未按预期工作，现修复。

--- translate/test_translator.py
+++ translate/test_translator.py
@@ -1,14 +1,34 @@
 from unittest import TestCase
-
 from parameterized import parameterized
 
-from translate.translator import Translator
-from util.reflection import scan_all_impl_class
+from translate import Translator, create_translator
+from util.reflection import scanAllImpl4Parameterized
+
+translate_en2zh_data_pos = [("one", "一"),
+                            ("six", "六"),
+                            ("apple", "苹果"),
+                            ("banana", "香蕉"), ]
+
+translate_en2zh_data_neg = [("two", "一"),
+                            ("six", "十"),
+                            ("orange", "苹果"),
+                            ("banana", "黄瓜"), ]
 
 
 class TestTranslator(TestCase):
-    @parameterized.expand(scan_all_impl_class(Translator))
-    def test_function_1(self, impl: Translator):
-        self.assertEqual(impl.translate("one"), "一")
-        self.assertEqual(impl.translate("six"), "六")
-        self.assertEqual(impl.translate("apple"), "苹果")
+    @parameterized.expand(scanAllImpl4Parameterized(Translator))
+    def test_translate(self, impl: Translator):
+        for en, zh in translate_en2zh_data_pos:
+            self.assertEqual(zh, impl.translate(en))
+        for en, zh in translate_en2zh_data_neg:
+            self.assertNotEqual(zh, impl.translate(en))
+
+    @parameterized.expand(translate_en2zh_data_pos)
+    def test_create_translator_pos(self, inp, expect):
+        with create_translator() as t:
+            self.assertEqual(expect, t.translate(inp))
+
+    @parameterized.expand(translate_en2zh_data_neg)
+    def test_create_translator_neg(self, inp, expect):
+        with create_translator() as t:
+            self.assertNotEqual(expect, t.translate(inp))
